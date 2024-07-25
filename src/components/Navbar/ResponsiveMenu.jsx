630f968e380482143e2bf4e061b8a095c05c5146
Shubham pandey <shubhamprakash170@gmail.com>
2024-07-25 20:55:19 +0530

    name in navbar

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -33,7 +33,7 @@ const ResponsiveMenu = ({ showMenu }) => {
       </div>
       <div className="footer">
         <h1>
-          Made with ❤ by <a href="https://dilshad-ahmed.github.io/">Dilshad</a>{" "}
+          Made with ❤ by <a href="https://dilshad-ahmed.github.io/">Shubham</a>{" "}
         </h1>
       </div>
     </div>
